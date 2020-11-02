ce9c108d7fcfb4db141ae4c4ed9f727d418af7f3
Andrea Manzini <ilmanzo@gmail.com>
2020-11-02 12:04:32 +0100

    exercise on structs  - take 2

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,13 +1,15 @@
 fn main() {
-    let width1 = 30;
-    let height1 = 50;
+    // let width1 = 30;
+    // let height1 = 50;
+
+    let dimensions=(30,50);
 
     println!(
         "The area of the rectangle is {} square pixels",
-        area(width1, height1)
+        area(dimensions)
     );
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(dim:(u32,u32)) -> u32 {
+    dim.0 * dim.1
 }
